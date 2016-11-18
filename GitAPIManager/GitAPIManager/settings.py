5f7b4754a0cc8467c03fcad3163e92c14fd5a366
Adriaan <Adriaan.Coetzee@viisnovis.co.za>
2016-11-18 15:11:10 +0200

    to test

--- GitAPIManager/GitAPIManager/settings.py
+++ GitAPIManager/GitAPIManager/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-#STATIC_ROOT = "/home/ascendedcrow/GitApiManager/GitAPIManager"
+STATIC_ROOT = "/home/ascendedcrow/GitApiManager/GitAPIManager"
 STATIC_URL = "/home/ascendedcrow/GitApiManager/GitAPIManager"
 
 STATICFILES_DIRS = (
