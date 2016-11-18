e0f22c98bd2fbd9445307b224a29d015aaf455db
Adriaan <Adriaan.Coetzee@viisnovis.co.za>
2016-11-18 14:56:24 +0200

    static info

--- GitAPIManager/GitAPIManager/settings.py
+++ GitAPIManager/GitAPIManager/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
